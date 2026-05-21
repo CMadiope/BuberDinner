12e1327a304661df6f750601b5f004fbe6c06346
CMadiope <confidencem97@gmail.com>
2026-05-21 20:50:35 +0200

    init commit

---
+++ BuberDinner.Api/Program.cs
@@ -0,0 +1,18 @@
+
+using BuberDinner.Application;
+using BuberDinner.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddApplication().AddInfrastructure(builder.Configuration);
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
---
+++ BuberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BuberDinner.Application.Common.Authentication;
+
+namespace BuberDinner.Application.Services.Authentication
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly IJwtTokenGenerator _jwtTokenGenerator;
+
+        public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
+        {
+            _jwtTokenGenerator = jwtTokenGenerator;
+        }
+
+        public AuthenticationResult Register(string firstName, string lastName, string email, string password)
+        {
+            // check if user exists
+
+            // Create user (generate unique id)
+
+            // Create JWT token
+            Guid userId = Guid.NewGuid();
+            var token = _jwtTokenGenerator.GenerateToken(userId, firstName, lastName);
+
+            return new AuthenticationResult(userId, firstName, lastName, email, token);
+        }
+        public AuthenticationResult Login(string email, string password)
+        {
+            return new AuthenticationResult(Guid.NewGuid(), "Confidence", "Madiope", email, "token");
+        }
+
+
+    }
+}
