28492379546aa306ddbdbada910b7924e9788d04
CMadiope <confidencem97@gmail.com>
2026-05-25 20:28:34 +0200

    feat: implement repository pattern

--- BuberDinner.Api/Controllers/AuthenticationController.cs
+++ BuberDinner.Api/Controllers/AuthenticationController.cs
@@ -21,14 +21,14 @@ namespace BuberDinner.Api.Controllers
         public IActionResult Register(RegisterRequest request)
         {
             var result = _authenticationService.Register(request.FirstName, request.LastName, request.Email, request.Password);
-            var response = new AuthenticationResponse(result.Id, result.FirstName, result.LastName, result.Email, result.Token);
+            var response = new AuthenticationResponse(result.user.Id, result.user.FirstName, result.user.LastName, result.user.Email, result.Token);
             return Ok(response);
         }
         [HttpPost("login")]
         public IActionResult Login(LoginRequest request)
         {
             var result = _authenticationService.Login(request.Email, request.Password);
-            var response = new AuthenticationResponse(result.Id, result.FirstName, result.LastName, result.Email, result.Token);
+            var response = new AuthenticationResponse(result.user.Id, result.user.FirstName, result.user.LastName, result.user.Email, result.Token);
             return Ok(response);
         }
     }
--- BuberDinner.Application/Services/Authentication/AuthenticationService.cs
+++ BuberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -3,33 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BuberDinner.Application.Common.Authentication;
+using BuberDinner.Application.Common.Interfaces.Persistence;
+using BuberDinner.Domain.Entities;
 
 namespace BuberDinner.Application.Services.Authentication
 {
     public class AuthenticationService : IAuthenticationService
     {
         private readonly IJwtTokenGenerator _jwtTokenGenerator;
+        private readonly IUserRepository _userRepository;
 
-        public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
+        public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator, IUserRepository userRepository)
         {
             _jwtTokenGenerator = jwtTokenGenerator;
+            _userRepository = userRepository;
         }
 
         public AuthenticationResult Register(string firstName, string lastName, string email, string password)
         {
             // check if user exists
+            if (_userRepository.GetUserByEmail(email) is not null)
+            {
+                throw new InvalidOperationException("User with this email already exists.");
+            }
 
             // Create user (generate unique id)
+            var user = new User
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                Email = email,
+                Password = password
+            };
+            _userRepository.Add(user);
 
             // Create JWT token
-            Guid userId = Guid.NewGuid();
-            var token = _jwtTokenGenerator.GenerateToken(userId, firstName, lastName);
+            var token = _jwtTokenGenerator.GenerateToken(user);
 
-            return new AuthenticationResult(userId, firstName, lastName, email, token);
+            return new AuthenticationResult(user, token);
         }
         public AuthenticationResult Login(string email, string password)
         {
-            return new AuthenticationResult(Guid.NewGuid(), "Confidence", "Madiope", email, "token");
+            // check if user exists
+            if (_userRepository.GetUserByEmail(email) is not User user)
+            {
+                throw new InvalidOperationException("User with this email does not exist.");
+            }
+
+            // check if password is correct
+            if (user.Password != password)
+            {
+                throw new InvalidOperationException("Invalid password.");
+            }
+
+
+            // Create JWT token
+            var token = _jwtTokenGenerator.GenerateToken(user);
+
+            return new AuthenticationResult(user, token);
         }
 
 
